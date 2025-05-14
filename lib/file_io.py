b222fb9a6cd173b9b26e67b031deb9b0c99f1970
Kane Kabena <onekaneldn@gmail.com>
2025-05-14 16:07:25 +0300

    file IO lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="test_file", file_content="This is a test content." ):
+    full_path = f"{file_name}.txt"
+    with open(full_path, "w", encoding='utf-8') as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="test_file", append_content="This is a test append content."):
+    full_path = f"{file_name}.txt"
+    with open(full_path, "a", encoding='utf-8') as file:
+        file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name="test_file"):
+    full_path = f"{file_name}.txt"
+    with open(full_path, "r", encoding='utf-8') as file:
+        content = file.read()
+    return content
